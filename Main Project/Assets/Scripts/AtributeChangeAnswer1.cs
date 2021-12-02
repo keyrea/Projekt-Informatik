299c97e248add3d0e6c53c518c99c82248050a5d
keyrea <kiruhink2@gmail.com>
2021-12-02 18:55:42 +0100

    Answer1/Answer2 Button
    Внёс изменения по скриптам. Четыре показателя инициализируются и изменяются 2-мя скриптами.

---
+++ Main Project/Assets/Scripts/AtributeChangeAnswer1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtributeChangeAnswer1 : AtributeInitialization // изменение 4х показателей при нажатии кнопки "Answer1", наследование от Atributeinizialization
+{
+    private int healthPoints = -5; // на сколько изменится показатель "Здоровье"
+    private int studyPoints = -20; // на сколько изменится показатель "Знания"
+    private float moneyPoints = -60.00f; // на сколько изменится показатель "Деньги"
+    private float timePoints = -10.00f; // на сколько изменится показатель "Время"
+
+    private int actH; /* при нажатии кнопки изменённый показатель не менялся, пришлось ввести эту переменную, которая присваивает значение соответствующего
+                       * показателся в теле метода Answer1. actH- actuall Health
+                       */
+    private int actS; // actuall Study
+    private float actM; // actuall Money
+    private float actTime; // actuall Time
+
+    public Text ShowHealthChange;
+    public Text ShowStudyChange;
+    public Text ShowMoneyChange;
+    public Text ShowTimeChange;
+
+    public void Answer1() /* метод Answer1- при нажатии на него должны изменяться 4 показателя
+                           *если один или несколько показателей не меняются, то им присвоится значение 0 при инициализации в начале скрипта.
+                           */
+    {
+        actH = Health;
+        actS = Study;
+        actM = Money;
+        actTime = Time;
+        actH = actH + healthPoints; /* при инициализации переменной, которая изменяет значение одного из 4 показателей, присвается отрицательное или положительное
+                                     * так что в выражениях, изменяющих значение показателя, присутствует всегда знак плюс вне зависимости от того, этот 
+                                     * показатель должен уменьшатся или увеличиваться
+                                     */
+        actS = actS + studyPoints;
+        actM = actM + moneyPoints;
+        actTime = actTime + timePoints;
+        ShowHealthChange.text = actH.ToString() + " %";
+        ShowStudyChange.text = actS.ToString();
+        ShowMoneyChange.text = actM.ToString() + " EUR";
+        ShowTimeChange.text = actTime.ToString();
+    }
+
+
+}
---
+++ Main Project/Assets/Scripts/AtributeInitialization.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtributeInitialization : MonoBehaviour // инициализация 4 показателей
+{
+    private int health = 100; // инициализация первоначального значения "Здоровье"
+    private int study = 100; // инициализация первоначального значения "Знания"
+    private float money = 1000.00f; // инициализация первоначального значения "Деньги"
+    private float time = 100.00f; // инициализация первоначального значения "Время"
+
+    public Text ShowHealth; // текстовое поле для значения "Здоровье"
+    public Text ShowStudy; // текстовое поле для значения "Знания"
+    public Text ShowMoney; // текстовое поле для значения "Деньги"
+    public Text ShowTime; // текстовое поле для значения "Время"
+
+    public int Health
+    {
+        get { return health; }
+        set { health = Health; }
+    }
+
+    public int Study
+    {
+        get { return study; }
+        set { study = Study; }
+    }
+
+    public float Money
+    {
+        get { return money; }
+        set { money = Money; }
+    }
+
+    public float Time
+    {
+        get { return time; }
+        set { time = Time; }
+    }
+
+    public void Awake() // при запуске сцены сразу должны отобразиться первоначальные значения 4 показателей
+    {
+        ShowHealth.text = Health.ToString() + " %";
+        ShowMoney.text = Money.ToString() + " EUR";
+        ShowStudy.text = Study.ToString();
+        ShowTime.text = Time.ToString();
+    }
+}
