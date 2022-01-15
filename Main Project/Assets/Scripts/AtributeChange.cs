50f57d15c9cec21444326a83d8e6b63dc4cc9229
Dayfob <semembaevalihan19@gmail.com>
2022-01-15 15:35:33 +0100

    Added function changeAtributes
    Добавил изменение текущих атрибутов при нажатии кнопок;
    Добавил условие окончания игры при атрибуте меньшем или равном 0, переход в главное меню и обнуляет текущие параметры;
    Изменил функцию кнопки "выход" в меню выбора темы вопроса, теперь она еще и обнуляет текущие параметры;
    Добавил в БД вопросы по здоровью и деньгам.

---
+++ Main Project/Assets/Scripts/AtributeChange.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mono.Data.Sqlite;
+using System.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtributeChange : MonoBehaviour 
+{
+    public GameObject myGameObject;
+    public Text get_current_day;
+    public Text get_current_mental; // текстовое поле для значения "Ментальное здоровье"
+    public Text get_current_health; // текстовое поле для значения "Здоровье"
+    public Text get_current_progress; // текстовое поле для значения "Знания"
+    public Text get_current_money; // текстовое поле для значения "Деньги"
+
+    public Text get_yes_mental; // текстовое поле для значения "да Ментальное здоровье"
+    public Text get_yes_health; // текстовое поле для значения "да Здоровье"
+    public Text get_yes_progress; // текстовое поле для значения "да Знания"
+    public Text get_yes_money; // текстовое поле для значения "да Деньги"
+    
+    public Text get_no_mental; // текстовое поле для значения "да Ментальное здоровье"
+    public Text get_no_health; // текстовое поле для значения "да Здоровье"
+    public Text get_no_progress; // текстовое поле для значения "да Знания"
+    public Text get_no_money; // текстовое поле для значения "да Деньги"
+
+    public void changeAtribute(string answer_type){// тип ответа да/нет
+        // новые текущие занчения параметров
+        string result = "";
+        int new_current_day = int.Parse(get_current_day.text.ToString()) + 1;
+        int new_current_mental = int.Parse(get_current_mental.text.ToString());
+        int new_current_health = int.Parse(get_current_health.text.ToString());
+        int new_current_progress = int.Parse(get_current_progress.text.ToString());
+        int new_current_money = int.Parse(get_current_money.text.ToString());
+
+        if (answer_type == "yes"){
+            new_current_mental   += int.Parse(get_yes_mental.text.ToString());
+            new_current_health   += int.Parse(get_yes_health.text.ToString());
+            new_current_progress += int.Parse(get_yes_progress.text.ToString());
+            new_current_money    += int.Parse(get_yes_money.text.ToString());
+        } else if (answer_type == "no"){
+            new_current_mental   += int.Parse(get_no_mental.text.ToString());
+            new_current_health   += int.Parse(get_no_health.text.ToString());
+            new_current_progress += int.Parse(get_no_progress.text.ToString());
+            new_current_money    += int.Parse(get_no_money.text.ToString());
+        }
+
+        // если текущее значение превысило 10, то опустить его до 10
+        if(new_current_mental > 10){new_current_mental = 10;}
+        if(new_current_health > 10){new_current_health = 10;}
+        if(new_current_progress > 10){new_current_progress = 10;}
+        if(new_current_money > 10){new_current_money = 10;} 
+
+        // если текущее значение опустилось до 0 или ниже, перейти в главное меню и обнулить атрибуты
+        if (new_current_mental <= 0 || new_current_health <= 0 ||
+            new_current_progress <= 0 || new_current_money <= 0){
+                myGameObject.GetComponent<ChangeScene>().NextSceneAndToDefault(0);
+        } else { //иначе перейти в меню выбора вопроса
+            result = MyDataBase.ExecuteQueryWithAnswer("UPDATE Player " + 
+                                    "SET day ="+ new_current_day + "," + 
+                                    "current_mental ="+ new_current_mental + "," + 
+                                    "current_health ="+ new_current_health + "," + 
+                                    "current_progress ="+ new_current_progress + "," + 
+                                    "current_money ="+ new_current_money + " " + 
+                                    "WHERE ID_player = 1;");
+            myGameObject.GetComponent<ChangeScene>().NextScene(1);
+        }                                                          
+    }
+}
--- Main Project/Assets/Scripts/ChangeScene.cs
+++ Main Project/Assets/Scripts/ChangeScene.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,18 @@ public class ChangeScene : MonoBehaviour
 {
     public void NextScene(int _sceneNumber) // Метод для перехода к следующей сцене. Аргумент _sceneNumber - номер сцены. Сцены и их номера можно найти в Build Settings
     {
+        SceneManager.LoadScene(_sceneNumber);
+    }
+
+    public void NextSceneAndToDefault(int _sceneNumber) // Метод для перехода к следующей сцене. Устанаваилвает значения параметров по умолчанию
+    {
+        string result = MyDataBase.ExecuteQueryWithAnswer("UPDATE Player " + 
+                                "SET day = 1," + 
+                                "current_mental = 5," + 
+                                "current_health = 5," + 
+                                "current_progress = 5," + 
+                                "current_money = 5 " + 
+                                "WHERE ID_player = 1;");
         SceneManager.LoadScene(_sceneNumber);
     }
 
