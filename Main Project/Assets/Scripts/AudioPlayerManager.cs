d474ea90f609aacd1ede39e7a6d809594745e608
Dayfob <semembaevalihan19@gmail.com>
2022-01-21 17:16:46 +0100

    some deletes

--- Main Project/Assets/Scripts/AudioPlayerManager.cs
+++ Main Project/Assets/Scripts/AudioPlayerManager.cs
@@ -3,21 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// public class MuteSoundBackground : MonoBehaviour
-// {
-//     public void MuteToggle (bool muted)
-//     {
-//         if (muted)
-//         {
-//             AudioListener.volume = 1;
-//         }
-//         else
-//         {
-//             AudioListener.volume = 0;
-//         }
-//     }
-// }
-
 public class AudioPlayerManager: MonoBehaviour
 {
       private static AudioPlayerManager instance = null;
