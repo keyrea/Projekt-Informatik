d72ce57b4b13b3dadd40717f7fd5142084e2aeae
Dayfob <semembaevalihan19@gmail.com>
2022-01-21 16:57:45 +0100

    Added BGmusic, removed unnecessary files

---
+++ Main Project/Assets/Scripts/AudioPlayerManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// public class MuteSoundBackground : MonoBehaviour
+// {
+//     public void MuteToggle (bool muted)
+//     {
+//         if (muted)
+//         {
+//             AudioListener.volume = 1;
+//         }
+//         else
+//         {
+//             AudioListener.volume = 0;
+//         }
+//     }
+// }
+
+public class AudioPlayerManager: MonoBehaviour
+{
+      private static AudioPlayerManager instance = null;
+      private AudioSource audio;
+
+      private void Awake()
+      {
+          if (instance == null)
+          { 
+               instance = this;
+               DontDestroyOnLoad(gameObject);
+               return;
+          }
+          if (instance == this) return; 
+          Destroy(gameObject);
+      }
+
+      void Start()
+      {
+         audio = GetComponent<AudioSource>();
+         audio.Play();
+      }
+}
